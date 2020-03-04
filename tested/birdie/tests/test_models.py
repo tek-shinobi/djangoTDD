afd08d7e85c06d5af646451db1164e8f770034a8
tek-shinobi <tek.shinobi.ninja@gmail.com>
2020-03-04 22:43:31 +0400

    added test and model implementation

--- tested/birdie/tests/test_models.py
+++ tested/birdie/tests/test_models.py
@@ -3,15 +3,17 @@ import pytest
 # pytestmark = pytest.mark.django_db
 
 from .. models import Post
-# @pytest.mark.django_db
-# def test_creation(post_factory):
-#     post_created = post_factory()
-#     assert post_created.id == 1, 'Should be 1'
+
+pytestmark = pytest.mark.django_db
 
 class TestPost:
     pytestmark = pytest.mark.django_db
     def test_creation(self, post_factory):
         post_created = post_factory()
         post = Post.objects.get(id = post_created.id)
-        print(f'body:{post.body}')
         assert post.id == 1, 'Should be 1'
+
+    def test_get_excerpt(self, post_factory):
+        post_created = post_factory()
+        post = Post.objects.get(id = post_created.id)
+        assert post.get_excerpt(5) == post_created.body[:5], 'Should be first few letters of bodyf'
